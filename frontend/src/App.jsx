03731d00639ffaa0d47c398fc8661826667c5d74
Mike Mitchell <mike_mitchell@outlook.com>
2025-08-07 18:36:46 -0600

    fixed button bugs

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,27 +14,36 @@ export default function App() {
   const [time, setTime] = useState(0);
   const [running, setRunning] = useState(false);
   const [inputTime, setInputTime] = useState(300);
+  const [connected, setConnected] = useState(false);
 
   useEffect(() => {
+    socket.on("connect", () => setConnected(true));
+    socket.on("disconnect", () => setConnected(false));
+
     socket.on("timer_update", ({ time, running }) => {
       setTime(time);
       setRunning(running);
     });
-    return () => socket.off("timer_update");
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("timer_update");
+    };
   }, []);
 
-  const startTimer = () => socket.emit("start_timer");
-  const pauseTimer = () => socket.emit("pause_timer");
-  const resetTimer = () => socket.emit("reset_timer");
-  const setTimer = () => socket.emit("set_timer", inputTime);
+  const startTimer = () => connected && socket.emit("start_timer");
+  const pauseTimer = () => connected && socket.emit("pause_timer");
+  const resetTimer = () => connected && socket.emit("reset_timer");
+  const setTimer = () => connected && socket.emit("set_timer", inputTime);
 
   return (
     <div className="controller">
       <div className="time">{formatAsHMS(time)}</div>
       <div className="buttons">
-        <button onClick={startTimer} disabled={running}>Start</button>
-        <button onClick={pauseTimer} disabled={!running}>Pause</button>
-        <button onClick={resetTimer}>Reset</button>
+        <button onClick={startTimer} disabled={!connected || running}>Start</button>
+        <button onClick={pauseTimer} disabled={!connected || !running}>Pause</button>
+        <button onClick={resetTimer} disabled={!connected}>Reset</button>
       </div>
       <div>
         <input
@@ -42,7 +51,7 @@ export default function App() {
           value={inputTime}
           onChange={(e) => setInputTime(Number(e.target.value))}
         />
-        <button onClick={setTimer}>Set Time (seconds)</button>
+        <button onClick={setTimer} disabled={!connected}>Set Time (seconds)</button>
       </div>
     </div>
   );
